37729b3ae9cc1266f2de8d7396d202add20f98e8
Audrey Winona Cassandra Alvarez <cassieavrz@gmail.com>
2023-06-09 21:37:11 +0800

    Unittest

--- Alvarez_Unittest.py
+++ Alvarez_Unittest.py
@@ -10,37 +10,41 @@ class FlaskAPITestCase(unittest.TestCase):
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
         db.create_all()
 
+
     def tearDown(self):
         db.session.remove()
         db.drop_all()
 
-    def test_create_client(self):
+
+    def test_client_data(self):
         response = self.client.post('/clients', json={"client_name": "John Doe", "contact_number": "1234567890"})
         data = response.get_json()
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data['client_name'], "John Doe")
         self.assertEqual(data['contact_number'], "1234567890")
 
-    def test_create_partner(self):
+
+    def test_partner_data(self):
         response = self.client.post('/partners', json={"partner_name": "ABC Company", "partner_type": "Type A"})
         data = response.get_json()
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data['partner_name'], "ABC Company")
         self.assertEqual(data['partner_type'], "Type A")
 
-    def test_create_event(self):
+
+    def test_event_data(self):
         response = self.client.post('/events', json={"event_name": "Birthday Party", "event_date": "2023-06-08", "event_cost": 1000, "client_id": 1, "venue_id": 1, "theme_id": 1})
         data = response.get_json()
         self.assertEqual(response.status_code, 200)
 
 
-    def test_create_theme(self):
+    def test_theme_data(self):
         response = self.client.post('/themes', json={"theme_code": 123, "theme_name": "Pool Party"})
         data = response.get_json()
         self.assertEqual(response.status_code, 200)
 
 
-    def test_create_venue(self):
+    def test_venue_data(self):
         response = self.client.post('/venues', json={"venue_address": "Palawan, P.P.C.", "venue_fee": 500})
         data = response.get_json()
         self.assertEqual(response.status_code, 200)
